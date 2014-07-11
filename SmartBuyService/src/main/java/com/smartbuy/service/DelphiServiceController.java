e6c35c7d6027c9e4eca6b702a7a682acd46f7bef
wellscui <wells.cui@gmail.com>
2014-07-11 16:01:45 -0400

    add smartbuyservice

---
+++ SmartBuyService/src/main/java/com/smartbuy/service/DelphiServiceController.java
@@ -0,0 +1,24 @@
+package com.smartbuy.service;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/DelphiService")
+public class DelphiServiceController {
+	
+	@RequestMapping(value="/VehicleStatus")
+	public VehicleStatus getStatus()
+	{
+		VehicleStatus status=new VehicleStatus();
+		status.setLatitude(40);
+		status.setLongtitude(-74);
+		status.setSpeed(64);
+		status.setRpm(2000);
+		return status;
+	}
+
+}
